5cdaf8c99444d3654ed93e95023966314d5d21fb
Brian Metzger <metzgerb@oregonstate.edu>
2018-08-15 12:14:32 -0500

    filter test

--- public/main.js
+++ public/main.js
@@ -173,9 +173,10 @@ $(function() {
 
   // Gets the 'X is typing' messages of a user
   const getTypingMessages = (data) => {
-    return $(".typing").filter(i => {
-      return $(this).data('username') === data.username;
-    });
+    return $('.message.typing');
+    //return $(".typing").filter(i => {
+      //return $(this).data('username') === data.username;
+    //});
   }
 
   // Gets the color of a username through our hash function
